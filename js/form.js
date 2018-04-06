4f31eae2b817e1e00179d05bcba2c509c8a950e9
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 09:36:33 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,36 +1,36 @@
-$(document).ready(function() {
-  $('#signup').validate({
-	rules:{
-	username:{
-		required:true,
-		rangelength:[3,16]
-	},
-	email:{
-		required:true,
-		email:true
-		},
-	password:{
-	    required:true,
-		rangelength:[6,16]
-	},
-	
-	confirm_password:{
-	equalTo:'#password'	
-	}
-	},
-	messages:{
-	username:{
-	required: "Field is empty",
-	rangelength: "Username must contain from 3 to 16 chars"	
-	},
-	email:{
-	required: "Field is empty",
-	email: "You have entered invalid e-mail"	
-	},
-	password:{
-	required: "Field is empty",
-	rangelength:"Password must contain from 6 to 16 chars"	
-	},
-	},
- });
+$(document).ready(function () {
+    $('#signup').validate({
+        rules: {
+            username: {
+                required: true,
+                rangelength: [3, 16]
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                rangelength: [6, 16]
+            },
+
+            confirm_password: {
+                equalTo: '#password'
+            }
+        },
+        messages: {
+            username: {
+                required: "Field is empty",
+                rangelength: "Username must contain from 3 to 16 chars"
+            },
+            email: {
+                required: "Field is empty",
+                email: "You have entered invalid e-mail"
+            },
+            password: {
+                required: "Field is empty",
+                rangelength: "Password must contain from 6 to 16 chars"
+            },
+        },
+    });
 });
