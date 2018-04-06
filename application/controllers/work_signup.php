b43059d3ca9a4929fb32a797d4d4bfcca15847e8
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 09:33:53 +0300

    Update work_signup.php

--- application/controllers/work_signup.php
+++ application/controllers/work_signup.php
@@ -1,22 +1,22 @@
 <?php
-	include 'controller.php';
-	include '../models/model.php';
-	
-	$get_signup = new Controller;
-		$get_signup->getDataSignup($username, $email, $password);
+include 'controller.php';
+include '../models/model.php';
 
-	$set_signup = new Model;
-		$set_username = $get_signup->username;
-		$set_email = $get_signup->email;
-		$set_password = $get_signup->password;
+$get_signup = new Controller;
+$get_signup->getDataSignup($username, $email, $password);
 
-			$set_signup->isEmptyFieldsSignup($set_username, $set_email, $set_password);
-			$set_signup->cleanFieldsSignup($set_username, $set_email, $set_password);
-			$set_signup->lengthUsername();
-			$set_signup->lengthPassword();
-			$set_signup->isValidateEmail();
+$set_signup = new Model;
+$set_username = $get_signup->username;
+$set_email = $get_signup->email;
+$set_password = $get_signup->password;
 
-			$set_signup->isUsernameOrEmailExist();
-			$set_signup->insertSignup();
-			$set_signup->sendConfirmMessage();
+$set_signup->isEmptyFieldsSignup($set_username, $set_email, $set_password);
+$set_signup->cleanFieldsSignup($set_username, $set_email, $set_password);
+$set_signup->lengthUsername();
+$set_signup->lengthPassword();
+$set_signup->isValidateEmail();
+
+$set_signup->isUsernameOrEmailExist();
+$set_signup->insertSignup();
+$set_signup->sendConfirmMessage();
 ?>
