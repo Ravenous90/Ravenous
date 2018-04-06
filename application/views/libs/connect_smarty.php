c9ba483066cfdc54c1ab1217a431679eefc9894e
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 09:21:15 +0300

    Update connect_smarty.php

--- application/views/libs/connect_smarty.php
+++ application/views/libs/connect_smarty.php
@@ -1,19 +1,18 @@
 <?php
-	require_once('Smarty.class.php');
-	$smarty = new Smarty();
+require_once('Smarty.class.php');
+$smarty = new Smarty();
 
-		$smarty->template_dir = 'libs/templates/';
-		$smarty->compile_dir = 'libs/templates_c/';
-		$smarty->cache_dir = 'libs/cache/';
-		$smarty->caching = false;
-	
-			include "/../content.php";
+$smarty->template_dir = 'libs/templates/';
+$smarty->compile_dir = 'libs/templates_c/';
+$smarty->cache_dir = 'libs/cache/';
+$smarty->caching = false;
 
-		$smarty->assign('title', $myContent->title);
-		$smarty->assign('header', $myContent->header);
-		$smarty->assign('footer', $myContent->footer);
-		$smarty->assign('maintext', $myContent->maintext);
-		$smarty->assign('guidelines', $myContent->guidelines);
-		$smarty->assign('contact', $myContent->contact);		// Загрузка контента и добавление в переменные Smarty
+include "/../content.php";
 
+$smarty->assign('title', $myContent->title);
+$smarty->assign('header', $myContent->header);
+$smarty->assign('footer', $myContent->footer);
+$smarty->assign('maintext', $myContent->maintext);
+$smarty->assign('guidelines', $myContent->guidelines);
+$smarty->assign('contact', $myContent->contact);        // Загрузка контента и добавление в переменные Smarty
 ?>
