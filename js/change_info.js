53110a62269556c419cfbb0e7e83ce0912598788
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 09:36:08 +0300

    Update change_info.js

--- js/change_info.js
+++ js/change_info.js
@@ -1,30 +1,29 @@
+$(document).ready(function () {
+    $("#edit_profile").click(
+        function () {
+            $('#modal').arcticmodal();
+        });
 
-$(document).ready(function() {
-$("#edit_profile").click(
-	function() {
-$('#modal').arcticmodal();
-	});
-	
-	
-	$("#edit").click(
-		function(){
-			sendAjaxForm('result_change', 'change', '../controllers/work_account.php');
-			return false;
-		}
-	);
+    $("#edit").click(
+        function () {
+            sendAjaxForm('result_change', 'change', '../controllers/work_account.php');
+            return false;
+        }
+    );
 });
+
 function sendAjaxForm(result_change, change, url) {
     $.ajax({
-        url:     url,
-        type:     "POST",
+        url: url,
+        type: "POST",
         dataType: "html",
-        data: $("#"+change).serialize(), 
-        success: function(data) {
-        	
-        	$('#result_change').html(data);
-    	},
-    	error: function(response) {
+        data: $("#" + change).serialize(),
+        success: function (data) {
+
+            $('#result_change').html(data);
+        },
+        error: function (response) {
             $('#result_change').html('Error, data did not send');
-    	}
- 	});
+        }
+    });
 }
