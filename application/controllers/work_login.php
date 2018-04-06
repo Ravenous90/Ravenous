faaa51d212e2bbef7a23d4be35e74beeabd4aa78
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 09:33:27 +0300

    Update work_login.php

--- application/controllers/work_login.php
+++ application/controllers/work_login.php
@@ -1,19 +1,19 @@
 <?php
-	include 'controller.php';
-	include '../models/model.php';
-	
-	$get_login = new Controller;
-		$get_login->getDataLogin($username, $password);
-	
-	$set_login = new Model;
-		$set_username = $get_login->username;
-		$set_password = $get_login->password;
-		
-		$set_login->isEmptyFieldsLogin($set_username, $set_password);
-		$set_login->cleanFieldsLogin($set_username, $set_password);
-		$set_login->lengthUsername();
-		$set_login->lengthPassword();
-		$set_login->selectRow();
-		
-		$set_login->testLogin();
+include 'controller.php';
+include '../models/model.php';
+
+$get_login = new Controller;
+$get_login->getDataLogin($username, $password);
+
+$set_login = new Model;
+$set_username = $get_login->username;
+$set_password = $get_login->password;
+
+$set_login->isEmptyFieldsLogin($set_username, $set_password);
+$set_login->cleanFieldsLogin($set_username, $set_password);
+$set_login->lengthUsername();
+$set_login->lengthPassword();
+$set_login->selectRow();
+
+$set_login->testLogin();
 ?>
