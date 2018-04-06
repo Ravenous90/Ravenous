813680cea90441a0ca9cf1a5ff0ee4821d6d5762
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 09:09:11 +0300

    Update content.php

--- application/views/content.php
+++ application/views/content.php
@@ -14,59 +14,51 @@ $myContent = new Content();
 $myContent->title = 'Main page';
 $myContent->header = 'header';
 $myContent->footer = 'footer';
-$myContent->maintext = '<p>СП «Радмиртех» в форме общества с ограниченной ответственностью было создано в 2001 году на 
-					базе группы предприятий, включающих в себя Научно-исследовательский институт радиотехнических 
-					измерений и НТК «Институт монокристаллов» (г.Харьков).
-					В настоящее время СП «Радмиртех» — это коллектив высокопрофессиональных специалистов, 
-					которые с помощью передового оборудования успешно решают задачи, связанные с разработками и серийным 
-					производством современных средств измерительной техники.
-					Целью деятельности СП «Радмиртех» является выпуск продукции, качество которой полностью отвечает 
-					ожиданиям и требованиям потребителей, обеспечивающей улучшение финансового положения предприятия, 
-					а также создание отношений доверия между нашим предприятием и потребителями посредством поставок качественных изделий.
-					</p>
-					<p>СП «Радмиртех» разрабатывает и серийно выпускает средства измерения, осуществляющие обработку, хранение, 
-					отображение информации об объеме и расходе газа в рабочих и стандартных условиях: комплексы измерительные 
-					на базе роторных счетчиков КВР, комплексы измерительные на базе турбинных счетчиков КВТ, а также корректоры 
-					объема газа серии ВЕГА.</p>
-					<p>Узлы учета газа на базе корректоров ВЕГА, а также комплексов КВР и КВТ могут быть оборудованы системой 
-					сбора и передачи данных, оборудование для которой также производится СП «РАДМИРТЕХ».
-					Разработанная СП «РАДМИРТЕХ» Автоматическая система сбора данных с узлов учета промышленных и бытовых 
-					потребителей газа уже несколько лет успешно эксплуатируется на объектах, входящих в сферы влияния 
-					газораспределительных компаний.</p>
-					<p>Оборудование СП «Радмиртех» широко представлено в газовой, газоперерабатывающей и других сферах 
-					промышленности и коммунального хозяйства для коммерческого или технологического учета газов.
-					Основными потребителями продукции СП «Радмиртех» являются газораспределительные компании, а также 
-					крупные предприятия потребители природного газа (предприятия химической, металлургической промышленности, 
-					а также жилищно-коммунального хозяйства).</p>';
-$myContent->guidelines = '<p>Построение сети сбора данных от счётчиков предполагает наличие
-					устойчивой связи между её ретрансляторами. Наилучшие результаты могут
-					быть получены при высокой плотности абонентов в сети, т.е. если например,
-					сеть установлена полностью в многоэтажном доме.
-					Время подключения радиомодуля к сети напрямую зависит от количества
-					возможных связей поэтому сеть с большей плотностью более стабильна.
-					Концентратор сети следует, по возможности, устанавливать в центре зоны
-					покрытия сети, что уменьшает количество уровней сети и увеличивает её
-					стабильность. Например, в случае размещения системы в девятиэтажном
-					четырехподъездном доме концентратор рекомендуется устанавливать во
-					втором или третьем подъезде на четвертом этаже, так чтобы антенна была на
-					высоте около двух метров от пола. В случае частного сектора, необходимо
-					согласовать размещение с техническим отделом.</p>';
-$myContent->contact = '<p><b>Адрес:</b><br>
-					Украина, 61037, г.Харьков, проспект Московский, 199<br>
-					http://radmirtech.com.ua<br><br>
-			
-					<b>Коммерческий отдел:</b><br>
-					т./ф. +38 (057) 727-53-35,<br>
-					т. +38 (057) 727-53-36<br>
-					моб. +38 (098) 400-97-17<br>
-					e-mail: kommerc@radmirtech.com.ua<br><br>
+$myContent->maintext = '<p><b>Gas marketing in Europe</b></p>
+<p>The main goals of Gazprom in the European market are retaining its leadership position, ensuring reliable gas supplies, and improving the efficiency of its marketing activities.
+European countries have been among the key consumers of Russian gas for over 40 years. Gazprom is the largest exporter of natural gas to the European market.
+Russia has provided reliable natural gas supplies to Europe for over 40 years
+Over the course of 2016, the Gazprom Group sold 228.3 billion cubic meters of gas to Europe (including both exports from Russia and sales of gas purchased by the Group abroad). The net revenue from gas sales (net of excise tax and customs duties) totaled RUB 2,140 billion.
+In 2016, Russian gas exports to Europe (under contracts of Gazprom Export and Gazprom Schweiz) reached a record high of 179.3 billion cubic meters, exceeding the 2015 figure by 19.9 billion cubic meters (12.5 per cent).
+Gazprom’s activities in the European gas market are underpinned by long-term contracts with oil-pegged prices and take-or-pay clauses. The Company also uses new forms of trade based on short- and medium-term sales, swap operations, and spot contracts.</p>
+<p><b>Unified export channel</b></p>
+<p>The unified export channel is the backbone of Gazprom’s export strategy. Pursuant to the Russian law on gas export, Gazprom has an exclusive right to export gas via gas pipelines. The law allows the Company to pursue a coordinated production and marketing policy and serves as an additional legal guarantee of reliable gas exports from Russia.
+Long-term contract system
+Gazprom exports gas to Central and Western Europe mostly under long-term contracts of up to 25 years, usually based on intergovernmental agreements.
+Long-term contracts with oil-pegged prices and take-or-pay clauses are fundamental to stable and sustainable gas supplies. No other contract can guarantee that producers and exporters will get returns on multibillion investments in major gas export projects and that importers will enjoy secure and uninterrupted gas supplies in the long term.</p>
+<p>At present, Gazprom’s portfolio of signed long-term contracts provides for gas sales to Europe in the amount exceeding 4 trillion cubic meters over the contract period.
+The main features of the long-term contracts are as follows:
+price formula taking into account the oil prices of the previous six to nine months;
+clauses forbidding the unilateral termination of contracts unless caused by prolonged force majeure events;
+take-or-pay terms for significant contracted volumes, stipulating that buyers should pay for all contracted gas, whether offtaken or not, but may later withdraw the unconsumed volumes at a surcharge upon receiving the minimum annual volumes contracted for the specified year.</p>
+<p>In essence, the long-term contracts are service contracts that allow buyers to exercise flexibility with regard to daily volumes and annual supplies, while the seller has an obligation to deliver the pre-paid take-or-pay volumes. Moreover, long-term contracts provide a guarantee of gas deliveries over a substantial period of time. Spot gas, 
+meanwhile, is a fundamentally different product, which makes direct comparisons between contract and spot prices unjustified.
+Oil-indexed contracts, however, continue to be relevant. Oil indexation is an essential means of long-term business planning for the benefit of both gas purchasers and sellers. It provides for investment continuity and stability in the gas sector in the vertical dimension from wells to end consumers. Oil indexation, which has proved to be useful 
+in the course of more than 40 years of global gas market development, is also employed by other major exporters. In current conditions, 
+oil products act as a universal deflator in the gas price formula, placing the gas price in the context of other feedstock prices.</p>';
+$myContent->guidelines = '<p><b>Enhancing reliability of gas supplies to Europe</b></p>
+<p>Gazprom implements a set of measures to enhance the reliability of its gas supplies to European consumers, including systematic efforts for contracting gas transmission capacities, optimizing and redistributing the contracted capacities, executing swap deals, and mitigating flow interruptions and other emergencies.
+With a view to further improve supply reliability, Gazprom initiated the Nord Stream, Nord Stream 2 and TurkStream gas transmission projects.
+Nord Stream, the first gas pipeline in history to establish a direct connection between the Russian and European gas transmission systems, reached its design capacity in 2012. Nord Stream 2 will be put in operation before the end of 2019.
+TurkStream is a new export gas pipeline stretching from Russia to Turkey across the Black Sea. The first string of the gas pipeline is intended for Turkish consumers, while the second string will deliver gas to southern and southeastern Europe. Construction of TurkStream started in 2017.
+The use of underground gas storage (UGS) facilities in Europe also helps considerably increase the stability of export supplies and boost Russian gas sales.</p>';
+$myContent->contact = '<p><b>Address:</b><br>
+16 Nametkina St., Moscow, GSP-7, 117997, Russian Federation</p>
 
-					<b>Отдел технической поддержки:</b><br>
-					т./ф. +38 (057) 727–53–32<br>
-					e-mail: tex@radmirtech.com.ua<br><br>
+<p><b>Phone:</b><br>
++7 495 719-30-01 (for inquiries)<br>
++7 495 719-83-33 (fax)<br>
++7 812 413-73-33 (fax)</p>
 
-					<b>Отдел поверки и ремонта оборудования:</b><br>
-					т./ф. +38 (057) 727-53-29,<br>
-					моб. +38 (066) 720-53-05<br>
-					e-mail: service@radmirtech.com.ua<p>';
+<p><b>E-mail:</b><br>
+gazprom@gazprom.ru<br>
+webmaster@gazprom.ru (for comments or remarks about the website)</p>
+
+<p><b>Mailing address:</b><br>
+BOX 1255, St. Petersburg, 190000, Russian Federation</p>
+
+<p><b>Teletype service:</b><br>
+111012 Синтин;<br>
+121805 GAZ RU;<br>
+121806 GAZ RU.</p>';
 ?>					
