e749caa2944f4e26133628240b25233b2632e3e5
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 08:48:31 +0300

    Update guidelines.php

--- application/views/guidelines.php
+++ application/views/guidelines.php
@@ -1,7 +1,7 @@
 <?php 
-	include "libs/connect_smarty.php";
-		$smarty->display('libs/templates/header.tpl');
-		$smarty->display('libs/templates/menu.tpl');
-		$smarty->display('libs/templates/guidelines.tpl');
-		$smarty->display('libs/templates/footer.tpl');
+include "libs/connect_smarty.php";
+$smarty->display('libs/templates/header.tpl');
+$smarty->display('libs/templates/menu.tpl');
+$smarty->display('libs/templates/guidelines.tpl');
+$smarty->display('libs/templates/footer.tpl');
 ?>
