da3f4a51ad7c8523fe0abd9efe718ed3ce982e62
Ravenous90 <36443991+Ravenous90@users.noreply.github.com>
2018-04-06 08:39:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-	include "application/models/config.php";
-		header("Location: ".$projectLocation."views/main.php");
+include "application/models/config.php";
+header("Location: ".$projectLocation."views/main.php");
 ?>
